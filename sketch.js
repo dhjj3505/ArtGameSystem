c99c4725a2adb7466ee7c88a7f233f59edfa01be
dhjj3505 <letsmin@naver.com>
2019-12-16 22:06:49 +0900

    EnemyAppearence

--- sketch.js
+++ sketch.js
@@ -1,13 +1,30 @@
-let canvas;
-let canvasWidth = 600;
+let canvasWidth = 800;
 let canvasHeight = 400;
+let value = false;
 
 function setup() {
-  canvas = createCanvas(canvasWidth, canvasHeight);
-  canvas.position(windowWidth/2 - canvasWidth/2, 20);
-
+  createCanvas(800, 400);
 }
 
 function draw() {
- 
+  canvas = createCanvas(canvasWidth, canvasHeight);
+  canvas.position(windowWidth/2 - canvasWidth/2, 20);
+  background(255, 255, 255);
+  textAlign(CENTER);
+  text('Game Start', width/2, height/2);
+  fill(0);
+
+  if(value === true) {
+    var s = createSprite(400, 200, 30, 30);
+    s.velocity.x = random(-5, 5);
+    s.velocity.y = random(-5, 5);
+  }
+
+  drawSprites();
+
 }
+
+function mousePressed() {
+  value = true;
+
+ }
